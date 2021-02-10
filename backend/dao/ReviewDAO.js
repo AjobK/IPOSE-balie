deeef9754262105eddfc03c4d2e00797d9ba8d0e
Mitchell1121792 <s1121792@student.hsleiden.nl>
2021-02-10 19:32:28 +0100

    Add ninja

--- backend/dao/ReviewDAO.js
+++ backend/dao/ReviewDAO.js
@@ -1,66 +1,79 @@
-const db = require('../database/db');
+const db = require("../database/db");
 
 module.exports = class ReviewDAO {
-    static getOpenReviews() {
-        return db.query(`
+  static getOpenReviews() {
+    return db.query(`
             SELECT r.id AS id, s.st_number AS student_number, r.request_time AS request_time, a.id AS assignment_id
             FROM review r
             LEFT JOIN student s ON r.student_id = s.id
             LEFT JOIN assignment a ON r.assignment_id = a.id
             WHERE reviewer_id IS NULL AND request_time >= CURRENT_DATE ORDER BY request_time;
         `);
-    }
-    
-    static getClosedReviews() {
-        return db.query('SELECT * FROM review WHERE reviewer_id IS NOT NULL ORDER BY request_time;');
-    }
+  }
 
-    static getAllReviews() {
-        return db.query('SELECT * FROM review ORDER BY request_time;');
-    }
+  static getClosedReviews() {
+    return db.query(
+      "SELECT * FROM review WHERE reviewer_id IS NOT NULL ORDER BY request_time;"
+    );
+  }
 
-    static async createReview(body) {
-        const { studentNumber, assignmentId } = body;
+  static getAllReviews() {
+    return db.query("SELECT * FROM review ORDER BY request_time;");
+  }
 
-        let studentId = -1;
+  static getReviewById(id) {
+    return db.query("SELECT * FROM review WHERE id = $1;", [id]);
+  }
 
-        await db.query('SELECT * FROM student WHERE st_number=$1;', [studentNumber])
-        .then((student) => {
-            if (student.rows.length <= 0)
-                throw new Error('This student is not allowed to open a review');
-            else
-                studentId = student.rows[0].id;
-        })
-        
-        let newDate = new Date().toLocaleString();
+  static async createReview(body) {
+    const { studentNumber, assignmentId } = body;
 
-        return db.query(`
+    let studentId = -1;
+
+    await db
+      .query("SELECT * FROM student WHERE st_number=$1;", [studentNumber])
+      .then((student) => {
+        if (student.rows.length <= 0)
+          throw new Error("This student is not allowed to open a review");
+        else studentId = student.rows[0].id;
+      });
+
+    let newDate = new Date().toLocaleString();
+
+    return db
+      .query(
+        `
             SELECT st_number, reviewer_id, request_time
             FROM student
             RIGHT JOIN review ON review.student_id = student.id
             WHERE st_number=$1 AND request_time >= CURRENT_DATE;
-        `, [studentNumber])
-        .then(async (students) => {
-            console.log(students.rows)
+        `,
+        [studentNumber]
+      )
+      .then(async (students) => {
+        console.log(students.rows);
 
-            for (let i = 0; i < students.rows.length; i++) {
-                if (students.rows[i].reviewer_id == null)
-                    throw new Error('This student has already opened a review');
-            }
+        for (let i = 0; i < students.rows.length; i++) {
+          if (students.rows[i].reviewer_id == null)
+            throw new Error("This student has already opened a review");
+        }
 
-            return await db.query(
-                'INSERT INTO review (student_id, assignment_id, request_time) VALUES ($1, $2, $3);',
-                [studentId, assignmentId, newDate]
-            )
-        })
-    }
+        return await db.query(
+          "INSERT INTO review (student_id, assignment_id, request_time) VALUES ($1, $2, $3);",
+          [studentId, assignmentId, newDate]
+        );
+      });
+  }
 
-    static deleteReplicaById(id) {
-        // Escape not needed, DB errors if invalid type
-        return db.query('DELETE FROM replica WHERE id=$1;', [id])
-    }
+  static deleteReplicaById(id) {
+    // Escape not needed, DB errors if invalid type
+    return db.query("DELETE FROM replica WHERE id=$1;", [id]);
+  }
 
-    static setReviewer(reviewId, reviewerId) {
-        return db.query('UPDATE review SET reviewer_id = $1 WHERE id = $2;', [reviewerId, reviewId]);
-    }
-}
+  static setReviewer(reviewId, reviewerId) {
+    return db.query("UPDATE review SET reviewer_id = $1 WHERE id = $2;", [
+      reviewerId,
+      reviewId,
+    ]);
+  }
+};
