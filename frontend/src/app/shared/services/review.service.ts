5e75e25991558a916593add6e75f1878d2606284
Mitchell1121792 <s1121792@student.hsleiden.nl>
2021-02-12 18:28:11 +0100

    Add getTakenReviews to review.service.ts (frontend)

--- frontend/src/app/shared/services/review.service.ts
+++ frontend/src/app/shared/services/review.service.ts
@@ -1,9 +1,6 @@
 import { Subject } from 'rxjs';
 import { Review } from '../models/review.model';
-import {
-    HttpClient,
-    HttpResponse
-} from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { first } from 'rxjs/operators';
@@ -11,102 +8,128 @@ import { AssignmentService } from './assignment.service';
 
 @Injectable()
 export class ReviewService {
-    private reviews: Review[] = [];
-    reviewsChanged = new Subject<Review[]>();
+  private reviews: Review[] = [];
+  reviewsChanged = new Subject<Review[]>();
 
-    constructor(
-        private http: HttpClient,
-        private assignmentService: AssignmentService
-    ) {
+  constructor(
+    private http: HttpClient,
+    private assignmentService: AssignmentService
+  ) {
+    this.reviewsChanged.next(this.reviews);
+    this.fetchReviews();
+
+    assignmentService.currentAssignmentChanged.subscribe(() => {
+      this.fetchReviews();
+    });
+  }
+
+  fetchReviews() {
+    this.http
+      .get<any>(
+        environment.API_URL + '/api/review',
+        environment.DEFAULT_HTTP_OPTIONS
+      )
+      .subscribe((res: HttpResponse<any>) => {
+        this.reviews = [];
+        res.body.reviews.map((review) => {
+          this.reviews.push(
+            new Review(
+              review.id,
+              review.student_number,
+              review.request_time,
+              review.assignment_id
+            )
+          );
+        });
+
+        this.reviews = this.reviews.filter((i) => {
+          return (
+            i.assignmentId ==
+            (this.assignmentService.currentAssignment
+              ? this.assignmentService.currentAssignment.id
+              : i.assignmentId)
+          );
+        });
+
         this.reviewsChanged.next(this.reviews);
-        this.fetchReviews();
+      });
+  }
 
-        assignmentService.currentAssignmentChanged.subscribe(() => {
-            this.fetchReviews();
-        })
-    }
+  setReviews(reviews: Review[]) {
+    this.reviews = reviews;
+  }
 
-    fetchReviews() {
-        this.http
-        .get<any>(environment.API_URL + '/api/review', environment.DEFAULT_HTTP_OPTIONS)
-        .subscribe(
-            (res: HttpResponse<any>) => {
-                this.reviews = [];
-                res.body.reviews.map((review) => {
-                    this.reviews.push(new Review(
-                        review.id,
-                        review.student_number,
-                        review.request_time,
-                        review.assignment_id
-                    ))
-                })
+  getReviews() {
+    return this.reviews.slice();
+  }
 
-                this.reviews = this.reviews.filter((i) => {
-                    return i.assignmentId == (this.assignmentService.currentAssignment ? this.assignmentService.currentAssignment.id : i.assignmentId)
-                });
+  getReview(index: number) {
+    return this.reviews[index];
+  }
 
-                this.reviewsChanged.next(this.reviews);
-            }
-        )
-    }
+  getReviewById(id: number) {
+    return this.http.get<any>(
+      environment.API_URL + `/api/review/${id}`,
+      environment.DEFAULT_HTTP_OPTIONS
+    );
+  }
 
-    setReviews(reviews: Review[]) {
-        this.reviews = reviews;
-    }
+  getTakenReviews(reviewerId) {
+    return this.http.get<any>(
+      environment.API_URL + `/api/review/taken/${reviewerId}`,
+      environment.DEFAULT_HTTP_OPTIONS
+    );
+  }
 
-    getReviews() {
-        return this.reviews.slice();
-    }
+  updateReviewById(
+    reviewData: {
+      artist: string;
+      name: string;
+      origin: string;
+      cost: number;
+      year: number;
+      imageUrl: string;
+    },
+    id: number
+  ) {
+    return this.http.put<any>(
+      environment.API_URL + `/api/review/${id}`,
+      reviewData,
+      environment.DEFAULT_HTTP_OPTIONS
+    );
+  }
 
-    getReview(index: number) {
-        return this.reviews[index];
-    }
+  createReview(reviewData: { studentNumber: string; assignmentId: number }) {
+    return this.http.post<any>(
+      environment.API_URL + `/api/review`,
+      reviewData,
+      environment.DEFAULT_HTTP_OPTIONS
+    );
+  }
 
-    getReviewById(id: number) {
-        return this.http
-        .get<any>(environment.API_URL + `/api/review/${id}`, environment.DEFAULT_HTTP_OPTIONS)
-    }
+  removeReview(review: Review) {
+    this.http
+      .delete<any>(
+        environment.API_URL + `/api/review/${review.id}`,
+        environment.DEFAULT_HTTP_OPTIONS
+      )
+      .pipe(first())
+      .subscribe((res: HttpResponse<any>) => {
+        for (let i = 0; i < this.reviews.length; i++) {
+          if (this.reviews[i].id == review.id) {
+            this.reviews.splice(i, 1);
+            this.reviewsChanged.next(this.reviews);
+            break;
+          }
+        }
+      });
+  }
 
-    updateReviewById(reviewData: {
-        artist: string,
-        name: string,
-        origin: string,
-        cost: number,
-        year: number,
-        imageUrl: string
-    }, id: number) {
-
-        return this.http
-        .put<any>(environment.API_URL + `/api/review/${id}`, reviewData, environment.DEFAULT_HTTP_OPTIONS)
-    }
-
-    createReview(reviewData: {
-        studentNumber: string,
-        assignmentId: number
-    }) {
-        return this.http
-        .post<any>(environment.API_URL + `/api/review`, reviewData, environment.DEFAULT_HTTP_OPTIONS)
-    }
-
-    removeReview(review: Review) {
-        this.http
-        .delete<any>(environment.API_URL + `/api/review/${review.id}`, environment.DEFAULT_HTTP_OPTIONS)
-        .pipe(first())
-        .subscribe(
-            (res: HttpResponse<any>) => {
-                for (let i = 0; i < this.reviews.length; i++) {
-                    if (this.reviews[i].id == review.id) {
-                        this.reviews.splice(i, 1);
-                        this.reviewsChanged.next(this.reviews);
-                        break;
-                    }
-                }
-            }
-        )
-    }
-
-    setAssigned(index: number) {
-        return this.http
-        .patch<any>(environment.API_URL + `/api/review/${index}`, {}, environment.DEFAULT_HTTP_OPTIONS);
-    }
+  setAssigned(index: number) {
+    return this.http.patch<any>(
+      environment.API_URL + `/api/review/${index}`,
+      {},
+      environment.DEFAULT_HTTP_OPTIONS
+    );
+  }
 }
