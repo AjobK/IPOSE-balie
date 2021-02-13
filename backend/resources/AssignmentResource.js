13cbc29f8011dc50f41c9c42313a4d6bcafc667f
Mitchell1121792 <s1121792@student.hsleiden.nl>
2021-02-13 19:51:11 +0100

    Add backend opening and closing of assignments

--- backend/resources/AssignmentResource.js
+++ backend/resources/AssignmentResource.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const assignmentController = require('../controllers/AssignmentController');
+const assignmentController = require("../controllers/AssignmentController");
 
 const router = express.Router();
 
 // GET /assignments
-router.get('/', assignmentController.getAssignments);
+router.get("/", assignmentController.getAssignments);
+
+// POST /assignments/open/:assignmentId
+router.get("/open/:assignmentId", assignmentController.openAssignment);
+
+// Post /assignments/close/:assignmentId
+router.get("/close/:assignmentId", assignmentController.closeAssignment);
 
 module.exports = router;
