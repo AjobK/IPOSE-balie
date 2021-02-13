a5f1219913b7854a59dac55520b029647da3dfe1
Mitchell1121792 <s1121792@student.hsleiden.nl>
2021-02-13 20:33:14 +0100

    Add functions for opening and closing assignments (frontend)

--- frontend/src/app/shared/services/assignment.service.ts
+++ frontend/src/app/shared/services/assignment.service.ts
@@ -1,54 +1,62 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Assignment } from '../models/assignment.model';
-import {
-    HttpClient,
-    HttpResponse
-} from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class AssignmentService {
-    currentAssignment: Assignment;
-    currentAssignmentChanged: Subject<Assignment> = new Subject<Assignment>();
-    assignments: Assignment[];
-    assignmentsChanged: Subject<Assignment[]> = new Subject<Assignment[]>();
+  currentAssignment: Assignment;
+  currentAssignmentChanged: Subject<Assignment> = new Subject<Assignment>();
+  assignments: Assignment[];
+  assignmentsChanged: Subject<Assignment[]> = new Subject<Assignment[]>();
 
-    constructor(
-        private http: HttpClient
-    ) {
+  constructor(private http: HttpClient) {
+    this.assignmentsChanged.next(this.assignments);
+    this.fetchAssignments();
+  }
+
+  setAssignment(assignment): void {
+    this.currentAssignment = assignment;
+    this.currentAssignmentChanged.next(this.currentAssignment);
+  }
+
+  openAssignment(id): void {
+    this.http.patch<any>(
+      environment.API_URL + `/api/assignmetns/close/${id}`,
+      environment.DEFAULT_HTTP_OPTIONS
+    );
+  }
+
+  closeAssignment(id): void {
+    this.http.patch<any>(
+      environment.API_URL + `/api/assignmetns/close/${id}`,
+      environment.DEFAULT_HTTP_OPTIONS
+    );
+  }
+
+  fetchAssignments() {
+    this.http
+      .get<any>(
+        environment.API_URL + '/api/assignments',
+        environment.DEFAULT_HTTP_OPTIONS
+      )
+      .subscribe((res: HttpResponse<any>) => {
+        this.assignments = [];
+        res.body.assignments.map((assignment) => {
+          this.assignments.push(
+            new Assignment(assignment.id, assignment.name, assignment.open)
+          );
+        });
+
         this.assignmentsChanged.next(this.assignments);
-        this.fetchAssignments();
-    }
 
-    setAssignment(assignment): void {
-        this.currentAssignment = assignment;
-        this.currentAssignmentChanged.next(this.currentAssignment);
-    }
-    
-    fetchAssignments() {
-        this.http
-        .get<any>(environment.API_URL + '/api/assignments', environment.DEFAULT_HTTP_OPTIONS)
-        .subscribe(
-            (res: HttpResponse<any>) => {
-                this.assignments = [];
-                res.body.assignments.map((assignment) => {
-                    this.assignments.push(new Assignment(
-                        assignment.id,
-                        assignment.name,
-                        assignment.open
-                    ))
-                })
-
-                this.assignmentsChanged.next(this.assignments);
-
-                if (this.assignments.length > 0) {
-                    this.currentAssignment = this.assignments[0];
-                    this.currentAssignmentChanged.next(this.currentAssignment);
-                }
-            }
-        )
-    }
+        if (this.assignments.length > 0) {
+          this.currentAssignment = this.assignments[0];
+          this.currentAssignmentChanged.next(this.currentAssignment);
+        }
+      });
+  }
 }
