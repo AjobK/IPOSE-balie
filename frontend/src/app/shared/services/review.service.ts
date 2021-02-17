b02977446f094d7d945852fc904d3060f46b7241
Ajob Kustra <ajobkustra.p@gmail.com>
2021-02-17 11:53:19 +0100

    fixed ninja again..

--- frontend/src/app/shared/services/review.service.ts
+++ frontend/src/app/shared/services/review.service.ts
@@ -163,7 +163,7 @@ export class ReviewService {
         },
         (e) => {
           alert(
-            (e.ninja ? `\n🐱‍👤 HAYAAA! Geninja\'d... \n\n\n Het overkomt ons allemaal \n\n\n❤️ Groetjes van ${e.ninja}` : false) ||
+            (e.error.ninja ? `\n🐱‍👤 HAYAAA! Geninja\'d... \n\n\n Het overkomt ons allemaal \n\n\n❤️ Groetjes van ${e.error.ninja}` : false) ||
             e.error.message ||
             'Oops, something went wrong...'
           );
